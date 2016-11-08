4b26b34419dcb9397dd484a638d86ad0570a13f0
TBartl <tbartl@umich.edu>
2016-11-08 16:14:00 -0500

    Fixed all the merge conflict bugs

--- Assets/InControl/Source/VersionInfo.cs
+++ Assets/InControl/Source/VersionInfo.cs
@@ -28,11 +28,7 @@ namespace InControl
 				Major = 1,
 				Minor = 4,
 				Patch = 4,
-<<<<<<< HEAD
-				Build = 3971
-=======
-				Build = 3970
->>>>>>> 3deea1843f86fb9b1cf2be45e627701148d6defb
+				Build = 3981
 			};
 		}
 
