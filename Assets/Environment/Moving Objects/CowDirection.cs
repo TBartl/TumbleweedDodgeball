dcee7d4c77c88659bf491850850563e4f350fc6f
Kyle Cooley <kylekc@umich.edu>
2016-11-11 11:21:03 -0500

    cow ready to fuck you up

---
+++ Assets/Environment/Moving Objects/CowDirection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CowDirection : MonoBehaviour {
+
+	Vector3 prevPosition;
+	CowMovement movement;
+
+	// Use this for initialization
+	void Start () {
+		prevPosition = transform.position;
+		movement = GetComponent<CowMovement>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (movement.currentSpeed != 0 && transform.position != prevPosition) {
+			transform.rotation = Quaternion.LookRotation(transform.position - prevPosition);
+			prevPosition = transform.position;
+		}
+	}
+}
