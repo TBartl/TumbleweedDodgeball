4994926b4c4bbd5bf14ad0b54013ae383e9b2425
Kyle Cooley <kylekc@umich.edu>
2016-11-12 12:48:33 -0500

    trail ball

---
+++ Assets/Ball/Variations/Trail/BallTrail.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallTrail : MonoBehaviour {
+
+	BallHotness hotness;
+	bool hot = false;
+	ParticleSystem trail;
+	EdgeCollider2D trailCollider;
+	Vector3 prevPosition;
+
+	// Use this for initialization
+	void Start () {
+		hotness = GetComponentInParent<BallHotness>();
+		trail = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hotness.GetIsHot() != hot) {
+			hot = hotness.GetIsHot();
+			if (hot) {
+				trail.Play();
+			}
+			else {
+				trail.Stop();
+			}
+		}
+	}
+}
---
+++ Assets/Ball/Variations/Trail/BallTrailCollider.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BallTrailCollider : MonoBehaviour {
+
+	BallHotness hotness;
+	Vector2 prevPosition;
+	List<Vector2> vertices;
+	ParticleSystem trail;
+	ParticleSystem.Particle[] particles;
+	List<PlayerMovement> playerMovements;
+
+	void Start() {
+		trail = GetComponent<ParticleSystem>();
+		particles = new ParticleSystem.Particle[trail.maxParticles];
+		//foreach ()
+	}
+
+	void Update() {
+		int numParticles = trail.GetParticles(particles);
+		for (int i = 0; i < numParticles; i += 5) {
+			CheckForCollision(particles[i]);
+		}
+	}
+
+	void CheckForCollision(ParticleSystem.Particle particle) {
+		foreach (GameObject player in PlayerManager.inst.players) {
+			if (Physics2D.Raycast(particle.position, player.transform.position - particle.position, 1f)) {
+				//player.GetComponent<PlayerMovement>().modifiers.Add(.5f);
+				//player.
+			}
+		}
+	}
+}
