9c8f3c6676fdab65286cb7310570c19983775eb4
Kyle Cooley <kylekc@umich.edu>
2016-11-16 16:26:39 -0500

    Started some camera adjusting

---
+++ Assets/Camera/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public Transform cameraTransform;
+
+	Transform[] playerTransforms;
+
+	// Use this for initialization
+	void Start () {
+		cameraTransform = GetComponent<Camera>().transform;
+		playerTransforms = new Transform[PlayerManager.inst.players.Count];
+		for (int i = 0; i < PlayerManager.inst.players.Count; ++i) {
+			playerTransforms[i] = PlayerManager.inst.players[i].transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cameraTransform.position = AveragePositions();
+	}
+
+	Vector3 AveragePositions() {
+		Vector3 average = Vector3.zero;
+		foreach (Transform transform in playerTransforms) {
+			average += transform.position;
+		}
+		average /= playerTransforms.Length;
+		average.z = cameraTransform.position.z;
+		return average;
+	}
+}
---
+++ Assets/Camera/CameraZoom.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour {
+
+	public float minZ, maxZ;
+	public float minXSep, maxXSep, minYSep, maxYSep;
+
+	public Transform cameraTransform;
+
+	Transform[] playerTransforms;
+
+	void Start() {
+		cameraTransform = GetComponent<Camera>().transform;
+		playerTransforms = new Transform[PlayerManager.inst.players.Count];
+		for (int i = 0; i < PlayerManager.inst.players.Count; ++i) {
+			playerTransforms[i] = PlayerManager.inst.players[i].transform;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		cameraTransform.position = new Vector3(cameraTransform.position.x, cameraTransform.position.y, getZ());
+	}
+
+	float getZ() {
+		float minX = float.MaxValue, maxX = float.MinValue, minY = float.MaxValue, maxY = float.MinValue;
+		foreach (Transform transform in playerTransforms) {
+
+			minX = Mathf.Min(minX, transform.position.x);
+			maxX = Mathf.Max(maxX, transform.position.x);
+
+			minY = Mathf.Min(minY, transform.position.y);
+			maxY = Mathf.Max(maxY, transform.position.y);
+		}
+
+		float xDif = Mathf.Abs(maxX - minX);
+		float yDif = Mathf.Abs(maxY - minY);
+
+		if (xDif <= minXSep) {
+			print(maxZ);
+			return maxZ;
+		}
+		if (xDif >= maxXSep) {
+			print(minZ);
+			return minZ;
+		}
+
+		print((minXSep + xDif) * (minZ - maxZ) / (maxXSep - minXSep) - minZ);
+		return (minXSep + xDif) * (minZ - maxZ) / (maxXSep - minXSep) - minZ;
+	}
+}
