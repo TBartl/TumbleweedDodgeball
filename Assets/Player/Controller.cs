db26c48882b73e2e2f74a1d801dda34379dff3f7
TBartl <tbartl@umich.edu>
2016-11-28 13:56:31 -0500

    Howdy

--- Assets/Player/Controller.cs
+++ Assets/Player/Controller.cs
@@ -153,28 +153,29 @@ public class Controller : MonoBehaviour {
         return inputDevice.Action1;
     }
 
-    public void Vibrate(float intensity) {
+	public void Vibrate(float intensity) {
 		if (!DebugManager.vibrationsEnabled)
 			return;
 
+		//Debug.Log(inputDevice.Meta);
 
 		inputDevice.Vibrate(intensity);
 
-        switch (inputDeviceNum) {
-            case 0:
-                GamePad.SetVibration(0, intensity, intensity);
-                break;
-            case 1:
-                GamePad.SetVibration((PlayerIndex)1, intensity, intensity);
-                break;
-            case 2:
-                GamePad.SetVibration((PlayerIndex)2, intensity, intensity);
-                break;
-            case 3:
-                GamePad.SetVibration((PlayerIndex)3, intensity, intensity);
-                break;
-        }
-    }
+		switch (inputDeviceNum) {
+			case 0:
+				GamePad.SetVibration(0, intensity, intensity);
+				break;
+			case 1:
+				GamePad.SetVibration((PlayerIndex)1, intensity, intensity);
+				break;
+			case 2:
+				GamePad.SetVibration((PlayerIndex)2, intensity, intensity);
+				break;
+			case 3:
+				GamePad.SetVibration((PlayerIndex)3, intensity, intensity);
+				break;
+		}
+	}
 
 	public bool GetConfirmDown() {
 		if (devMode || InputManager.Devices.Count <= inputDeviceNum) { // use keyboard & mouse
