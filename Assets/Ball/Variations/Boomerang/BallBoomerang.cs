dca7c5789249fdda0144ce11267cb656881aa4a1
Nathan Stanifer <npst@0587369003.wireless.umich.net>
2016-11-30 16:03:28 -0500

    working boomerang

--- Assets/Ball/Variations/Boomerang/BallBoomerang.cs
+++ Assets/Ball/Variations/Boomerang/BallBoomerang.cs
@@ -2,34 +2,46 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class BallBoomerang : MonoBehaviour {
+public class BallBoomerang  : BallHotness {
 
-	private bool returning = false;
-	private GameObject thrower;
+	public float speedBoost = -10f;
+	GameObject thrower = null;
+	private float maxDistance = 10f;
 
-	Rigidbody2D rigid;
-	BallSource ballSource;
+	bool returning = false;
 
-	void Awake() {
-		rigid = this.GetComponent<Rigidbody2D>();
-		ballSource = this.GetComponent<BallSource>();
-	}
-	
-
-	void Update () {
+	void Update() {
+		if (thrower == null) thrower = FindPlayer();
 		if (returning) {
-			Vector3 dir = (PlayerManager.inst.players[ballSource.GetThrower().num].transform.position - this.transform.position).normalized;
-			rigid.velocity = dir * rigid.velocity.magnitude;
+			rigid.velocity = (thrower.transform.position - this.transform.position).normalized * (rigid.velocity.magnitude + speedBoost);
 		} else {
-			//nothing for now
+			if (thrower == null) thrower = FindPlayer();
+			if (thrower != null && Mathf.Abs(Vector2.Distance(thrower.transform.position, this.transform.position)) > maxDistance) returning = true; 
 		}
 	}
 
-	void OnCollisionEnter2D(Collision2D other)
-	{
-		if (returning) Destroy(this.gameObject);
-		else {
+	void OnCollisionEnter2D(Collision2D other) {	
+		if (thrower != null && !returning) returning = true;
+		else if (thrower != null) Destroy(this.gameObject);
+	}
+
+	protected override void OnHitOther(GameObject other) {
+		// reverse direction if it wasnt returning so it returns
+		if (!returning) {
 			returning = true;
+			thrower = FindPlayer();
+			if (thrower != null)
+				rigid.velocity = (thrower.transform.position - this.transform.position).normalized * (rigid.velocity.magnitude + speedBoost);
+		} else Destroy(this.gameObject);
+	}
+
+	GameObject FindPlayer() {
+		GameObject closestPlayer = null;
+		foreach (GameObject player in PlayerManager.inst.players) {
+			if (source.GetThrower() == player.GetComponent<PlayerData>()) {
+				closestPlayer = player;
+			}
 		}
+		return closestPlayer;
 	}
 }
