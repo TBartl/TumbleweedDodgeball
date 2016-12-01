e8409e3a6b0493adde3fa5dc98381f23a6126aa6
Kyle Cooley <kylekc@umich.edu>
2016-12-01 15:40:28 -0500

    item spawning going good

---
+++ Assets/Environment/Item Spawners/ItemSpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemSpawner : MonoBehaviour {
+
+	public GameObject itemPrefab;
+	public Vector3 offset;
+	public float timeBetweenDropsMin, timeBetweenDropsMax;
+	float nextDropTime;
+	public Vector2 initialForce;
+
+	// Use this for initialization
+	void Start () {
+		GenerateNextDropTime();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time >= nextDropTime) {
+			GameObject item = Instantiate(itemPrefab);
+			item.transform.position = new Vector3(transform.position.x, transform.position.y, 0)
+				+ transform.rotation * offset;
+			Rigidbody2D itemBody = item.GetComponent<Rigidbody2D>();
+			if (itemBody != null) {
+				itemBody.AddForce(initialForce);
+			}
+			GenerateNextDropTime();
+		}
+	}
+
+	void GenerateNextDropTime() {
+		nextDropTime = Time.time + Random.Range(timeBetweenDropsMin, timeBetweenDropsMax);
+	}
+}
