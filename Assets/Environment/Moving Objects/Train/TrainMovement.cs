d6b724d54e059c6cd78c95afb203368c07b15a7e
Kyle Cooley <kylekc@umich.edu>
2016-11-30 20:51:55 -0500

    choo choo motherfuckers

--- Assets/Environment/Moving Objects/Train/TrainMovement.cs
+++ Assets/Environment/Moving Objects/Train/TrainMovement.cs
@@ -3,32 +3,43 @@ using System.Collections;
 
 public class TrainMovement : MonoBehaviour {
 
-	public float speed, timeBetween, offscreenY;
-	float startTime;
-	bool running = false;
-	public Vector3 startPos;
+	public float speed;
+		//, timeBetween, offscreenY;
+	//float startTime;
+	//bool running = false;
+	//public Vector3 startPos;
+	Vector3[] path;
+	float percent = 0;
+	public string pathName;
 
 
 	// Use this for initialization
 	void Start () {
-		startTime = Time.time + timeBetween;
+		path = iTweenPath.GetPath(pathName);
+		iTween.PutOnPath(gameObject, path, percent);
+		//startTime = Time.time + timeBetween;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (running) {
-			transform.position = new Vector3(transform.position.x, 
-				transform.position.y - speed * Time.deltaTime, transform.position.z);
-			if (transform.position.y <= offscreenY) {
-				running = false;
-				startTime = Time.time + timeBetween;
-			}
+		percent += speed * Time.deltaTime;
+		if (percent > 1) {
+			percent -= 1;
 		}
-		else {
-			if (Time.time >= startTime) {
-				running = true;
-				transform.position = startPos;
-			}
-		}
+		iTween.PutOnPath(gameObject, path, percent);
+		//if (running) {
+		//	transform.position = new Vector3(transform.position.x, 
+		//		transform.position.y - speed * Time.deltaTime, transform.position.z);
+		//	if (transform.position.y <= offscreenY) {
+		//		running = false;
+		//		startTime = Time.time + timeBetween;
+		//	}
+		//}
+		//else {
+		//	if (Time.time >= startTime) {
+		//		running = true;
+		//		transform.position = startPos;
+		//	}
+		//}
 	}
 }
--- Assets/Player/PlayerData.cs
+++ Assets/Player/PlayerData.cs
@@ -2,6 +2,6 @@
 using System.Collections;
 
 public class PlayerData : MonoBehaviour {
-	[HideInInspector]
+	//[HideInInspector]
     public int num;
 }
