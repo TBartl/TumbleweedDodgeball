35f265d35e955a9d323736b5a59c270e2ceb18eb
Kyle Cooley <kylekc@umich.edu>
2016-12-10 15:40:48 -0500

    particle effect on balls, looks sick af

---
+++ Assets/Ball/BallParticleSystem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallParticleSystem : MonoBehaviour {
+
+	public float numFrames;
+	Color color = Color.white;
+	ParticleSystem particles;
+
+	// Use this for initialization
+	void Start () {
+		particles = GetComponent<ParticleSystem>();
+		StartCoroutine(StartParticles());
+	}
+
+
+	public void SetColor(Color colorIn) {
+		color = colorIn;
+	}
+
+	IEnumerator StartParticles() {
+		particles.startColor = color;
+		particles.Play();
+		for (int i = 0; i < numFrames; ++i) {
+			yield return null;
+		}
+		particles.Stop();
+		StartCoroutine(DestroyParticlesWhenFinished());
+	}
+
+	IEnumerator DestroyParticlesWhenFinished() {
+		while (particles.particleCount > 0) {
+			yield return null;
+		}
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Ball/BallParticles.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallParticles : MonoBehaviour {
+
+	BallHotness hotness;
+	BallSource source;
+	public GameObject particlesPrefab;
+
+	void Start() {
+		hotness = GetComponent<BallHotness>();
+		source = GetComponent<BallSource>();
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+
+		if (hotness.GetIsHot()) {
+			if (coll.collider.tag == "Player") {
+				GameObject particles = Instantiate(particlesPrefab);
+				particles.transform.position = transform.position;
+				particles.GetComponent<BallParticleSystem>().SetColor(
+					PlayerManager.inst.GetColor(source.GetThrower().num));
+			}
+		}
+	}
+
+	//IEnumerator PlayParticles() {
+	//	particles.startColor = PlayerManager.inst.GetColor(source.GetThrower().num);
+	//	particles.Play();
+	//	for (int i = 0; i < numFrames; ++i) {
+	//		yield return null;
+	//	}
+	//	particles.Stop();
+	//}
+}
