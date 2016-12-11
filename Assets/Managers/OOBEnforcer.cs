9195fd8b05927e8d5eb88e9a6e3f79df9e716b8c
TBartl <tbartl@umich.edu>
2016-12-11 01:42:54 -0500

    Added a fail-safe for if players get out of bounds

---
+++ Assets/Managers/OOBEnforcer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OOBEnforcer : MonoBehaviour {
+
+	void OnTriggerStay2D(Collider2D coll) {
+		Transform root = coll.transform.root.transform;
+		root.position += -root.transform.position.normalized;
+
+	}
+}
