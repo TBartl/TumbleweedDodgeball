c4bea494ee34331b4d35fc77d5fea4b8806eea65
Kyle Cooley <kylekc@umich.edu>
2016-12-14 11:45:14 -0500

    juiced up level select a bit

---
+++ Assets/UI/SlowRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowRotate : MonoBehaviour {
+
+	float rotationSpeed = 20;
+	float currentRotation = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.AngleAxis(currentRotation, new Vector3(0, 0, 1));
+		currentRotation += rotationSpeed * Time.deltaTime;
+		if (currentRotation >= 360) {
+			currentRotation -= 360;
+		}
+	}
+}
