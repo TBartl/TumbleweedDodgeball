895034a6363991fdbed375fb6c539f1ebe7f56c6
TBartl <tbartl@umich.edu>
2016-12-16 16:31:03 -0500

    Fixed OOB enforcer

--- Assets/Managers/OOBEnforcer.cs
+++ Assets/Managers/OOBEnforcer.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class OOBEnforcer : MonoBehaviour {
 
 	void OnTriggerStay2D(Collider2D coll) {
-		Transform root = coll.transform.root.transform;
-		root.position += -root.transform.position.normalized;
+		if (coll.tag != "Player" || coll.tag != "Ball") {
+			Transform root = coll.transform.root.transform;
+			root.position += -root.transform.position.normalized * 7f;
+		}
 
 	}
 }
