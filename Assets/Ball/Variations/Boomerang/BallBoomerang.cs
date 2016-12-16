e4854c5e3d53809d5122ff35fc95a0e1d23979ab
Nathan Stanifer <npst@0587338833.wireless.umich.net>
2016-12-16 12:29:32 -0500

    boomerang ball works better

--- Assets/Ball/Variations/Boomerang/BallBoomerang.cs
+++ Assets/Ball/Variations/Boomerang/BallBoomerang.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class BallBoomerang  : BallHotness {
 
-	public float speedBoost = 0f;
+	private float speedBoost = 18f;
 	GameObject thrower = null;
 	private float maxDistance = 40f;
 	public Ball ball;
@@ -18,7 +18,8 @@ public class BallBoomerang  : BallHotness {
 	void Update() {
 		if (thrower == null) thrower = FindPlayer();
 		if (returning) {
-			rigid.velocity = (thrower.transform.position - this.transform.position).normalized * (rigid.velocity.magnitude + speedBoost);
+			rigid.velocity = (thrower.transform.position - this.transform.position).normalized * (speedBoost);
+			if (Mathf.Abs(Vector3.Distance(thrower.transform.position, this.transform.position)) < 1f) Destroy(this.gameObject);
 		} else {
 			if (thrower == null) thrower = FindPlayer();
 			if (thrower != null && Mathf.Abs(Vector2.Distance(thrower.transform.position, this.transform.position)) > maxDistance) returning = true; 
